fb1a22b6d125126d99d1cfc27f7656113521c61a
Jose David Rodriguez Velasco <jodarove@gmail.com>
2025-11-04 15:52:59 -0500

    fix: temporary fix to unlock testing

--- examples/platform-state-managers/jest.config.js
+++ examples/platform-state-managers/jest.config.js
@@ -2,5 +2,11 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
 module.exports = {
     ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+    modulePathIgnorePatterns: ['<rootDir>/.localdevserver'],
+    transformIgnorePatterns: [
+        jestConfig.transformIgnorePatterns?.[0]?.replace(
+            /(.*@salesforce\/sfdx-lwc-jest\/src\/lightning-stubs)/,
+            '$1|.*@lwc/state'
+        ) || '/node_modules/(?!(.*@salesforce/sfdx-lwc-jest/src/lightning-stubs|.*@lwc/state)/)'
+    ]
 };
